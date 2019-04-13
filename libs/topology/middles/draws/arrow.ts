471d555e9341a96cae0b6ff943864cee76e9cf57
Alsmile <alsmile123@qq.com>
2019-04-13 23:46:52 +0800

    space

--- libs/topology/middles/draws/arrow.ts
+++ libs/topology/middles/draws/arrow.ts
@@ -2,19 +2,13 @@ import { Rect } from '../../models/rect';
 
 export function arrow(ctx: CanvasRenderingContext2D, node: Rect) {
   ctx.beginPath();
-  
+
   ctx.moveTo(node.x, (node.y + (node.height / 3 + 0.5)) << 0);
-  
   ctx.lineTo((node.x + (node.width - node.height / 2 + 0.5)) << 0, (node.y + (node.height / 3 + 0.5)) << 0);
-  
   ctx.lineTo((node.x + (node.width - node.height / 2 + 0.5)) << 0, node.y);
-  
   ctx.lineTo(node.x + node.width, (node.y + (node.height / 2 + 0.5)) << 0);
-  
   ctx.lineTo((node.x + (node.width - node.height / 2 + 0.5)) << 0, node.y + node.height);
-  
   ctx.lineTo((node.x + (node.width - node.height / 2 + 0.5)) << 0, (node.y + ((node.height * 2) / 3 + 0.5)) << 0);
-  
   ctx.lineTo(node.x, (node.y + ((node.height * 2) / 3 + 0.5)) << 0);
   ctx.closePath();
   ctx.stroke();
--- libs/topology/middles/draws/diamond.ts
+++ libs/topology/middles/draws/diamond.ts
@@ -2,13 +2,10 @@ import { Rect } from '../../models/rect';
 
 export function diamond(ctx: CanvasRenderingContext2D, node: Rect) {
   ctx.beginPath();
-  
+
   ctx.moveTo((node.x + (node.width / 2 + 0.5)) << 0, node.y);
-  
   ctx.lineTo(node.x + node.width, (node.y + (node.height / 2 + 0.5)) << 0);
-  
   ctx.lineTo((node.x + (node.width / 2 + 0.5)) << 0, node.y + node.height);
-  
   ctx.lineTo(node.x, (node.y + (node.height / 2 + 0.5)) << 0);
   ctx.closePath();
   ctx.stroke();
